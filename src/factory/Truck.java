b1de623a8223a6c56779041f0d339f7592d03f7b
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-15 08:49:42 +0500

    Update Truck.java

--- src/factory/Truck.java
+++ src/factory/Truck.java
@@ -1,4 +1,22 @@
 package factory;
 
-public class Truck {
+public class Truck implements IVehicle {
+
+    private int capacity;
+    private int axles;
+
+    public Truck(int capacity, int axles) {
+        this.capacity = capacity;
+        this.axles = axles;
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Truck driving, capacity: " + capacity + " tons");
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println("Refueling truck with " + axles + " axles");
+    }
 }
