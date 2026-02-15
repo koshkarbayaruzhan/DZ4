5a6033cd65262dd5f58b6870d82f831b4c810678
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-15 08:51:03 +0500

    Update MotorcycleFactory.java

--- src/factory/MotorcycleFactory.java
+++ src/factory/MotorcycleFactory.java
@@ -1,4 +1,17 @@
 package factory;
 
-public class MotorcycleFactory {
+public class MotorcycleFactory extends VehicleFactory {
+
+    private String type;
+    private int engineVolume;
+
+    public MotorcycleFactory(String type, int engineVolume) {
+        this.type = type;
+        this.engineVolume = engineVolume;
+    }
+
+    @Override
+    public IVehicle createVehicle() {
+        return new Motorcycle(type, engineVolume);
+    }
 }
