d034576ecd6fbb8b8472c6116ab08801fc9653bc
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-15 08:49:15 +0500

    Update Motorcycle.java

--- src/factory/Motorcycle.java
+++ src/factory/Motorcycle.java
@@ -1,4 +1,22 @@
 package factory;
 
-public class Motorcycle {
+public class Motorcycle implements IVehicle {
+
+    private String type;
+    private int engineVolume;
+
+    public Motorcycle(String type, int engineVolume) {
+        this.type = type;
+        this.engineVolume = engineVolume;
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Motorcycle " + type + " is driving");
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println("Refueling motorcycle, engine: " + engineVolume + "cc");
+    }
 }
