3241b69e74fdbe5ccf446e3ce40166482f55e177
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-15 08:51:23 +0500

    Update TruckFactory.java

--- src/factory/TruckFactory.java
+++ src/factory/TruckFactory.java
@@ -1,4 +1,17 @@
 package factory;
 
-public class TruckFactory {
+public class TruckFactory extends VehicleFactory {
+
+    private int capacity;
+    private int axles;
+
+    public TruckFactory(int capacity, int axles) {
+        this.capacity = capacity;
+        this.axles = axles;
+    }
+
+    @Override
+    public IVehicle createVehicle() {
+        return new Truck(capacity, axles);
+    }
 }
