cbd50b47350ca02af12ac4dad345b14ead0bec54
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-15 08:50:05 +0500

    Update Bus.java

--- src/factory/Bus.java
+++ src/factory/Bus.java
@@ -1,4 +1,20 @@
 package factory;
 
-public class Bus {
+public class Bus implements IVehicle {
+
+    private int seats;
+
+    public Bus(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Bus driving, seats: " + seats);
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println("Refueling bus");
+    }
 }
