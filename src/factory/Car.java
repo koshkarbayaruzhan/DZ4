177a018bc3b65d3a5da62a28e46f3bc8a4685a2e
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-15 08:48:45 +0500

    Update Car.java

--- src/factory/Car.java
+++ src/factory/Car.java
@@ -1,4 +1,24 @@
 package factory;
 
-public class Car {
+public class Car implements IVehicle {
+
+    private String brand;
+    private String model;
+    private String fuelType;
+
+    public Car(String brand, String model, String fuelType) {
+        this.brand = brand;
+        this.model = model;
+        this.fuelType = fuelType;
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Car " + brand + " " + model + " is driving");
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println("Refueling car with " + fuelType);
+    }
 }
