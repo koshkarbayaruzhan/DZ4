77f405ed6c0faf18843a8bb35593a9395ffe1425
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-15 08:53:47 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,61 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package factory;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Choose vehicle: 1-Car  2-Motorcycle  3-Truck  4-Bus");
+        int choice = sc.nextInt();
+        sc.nextLine();
+
+        VehicleFactory factory;
+
+        switch (choice) {
+
+            case 1 -> {
+                System.out.print("Brand: ");
+                String brand = sc.nextLine();
+                System.out.print("Model: ");
+                String model = sc.nextLine();
+                System.out.print("Fuel: ");
+                String fuel = sc.nextLine();
+                factory = new CarFactory(brand, model, fuel);
+            }
+
+            case 2 -> {
+                System.out.print("Type: ");
+                String type = sc.nextLine();
+                System.out.print("Engine volume: ");
+                int volume = sc.nextInt();
+                factory = new MotorcycleFactory(type, volume);
+            }
+
+            case 3 -> {
+                System.out.print("Capacity (tons): ");
+                int cap = sc.nextInt();
+                System.out.print("Axles: ");
+                int ax = sc.nextInt();
+                factory = new TruckFactory(cap, ax);
+            }
+
+            case 4 -> {
+                System.out.print("Seats: ");
+                int seats = sc.nextInt();
+                factory = new BusFactory(seats);
+            }
+
+            default -> {
+                System.out.println("Wrong choice");
+                return;
+            }
         }
+
+        IVehicle vehicle = factory.createVehicle();
+        vehicle.drive();
+        vehicle.refuel();
     }
 }
