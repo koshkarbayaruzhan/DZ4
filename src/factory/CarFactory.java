cd4e40b102e0be219d764448ca7de20abd9818ff
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-15 08:50:44 +0500

    Update CarFactory.java

--- src/factory/CarFactory.java
+++ src/factory/CarFactory.java
@@ -1,4 +1,19 @@
 package factory;
 
-public class CarFactory {
+public class CarFactory extends VehicleFactory {
+
+    private String brand;
+    private String model;
+    private String fuelType;
+
+    public CarFactory(String brand, String model, String fuelType) {
+        this.brand = brand;
+        this.model = model;
+        this.fuelType = fuelType;
+    }
+
+    @Override
+    public IVehicle createVehicle() {
+        return new Car(brand, model, fuelType);
+    }
 }
